115112f698344ac5d91ee576b0fc5edd7a59dc1f
jtnebra <jotonesan@gmail.com>
2022-11-23 14:06:26 +0100

    multiplicacion

--- multiplicacion.java
+++ multiplicacion.java
@@ -1,9 +1,17 @@
 package calculadora;
-
+import.java.util.Scanner
 public class multiplicacion {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
+try(Scanner teclado= new Scanner(System.in)){
+int a, b, producto;
+System.out.print("dime el primer numero")
+a=teclado.nextInt();
+System.out.print("dime el segundo numero")
+b=teclado.nextInt();
+producto=a*b;
+System.out.println("el resultado de la multiplicacion es: " + producto)
+		}
 
 	}
 
